7a9bc22c2599decbe58346f6913628a817dd66ba
Ayesha-Babar <ayesha.babar@emumba.com>
2023-09-02 19:53:31 +0500

    till chapter 11

---
+++ Java Tutorials/Chapter 11/Shape.java
@@ -0,0 +1,14 @@
+public abstract class Shape
+{
+    // Abstraction -tutorial 11a
+
+     abstract double calculateArea();
+
+     //no body of an abstract class
+
+    public void print()
+    {
+        System.out.println("print method");
+    }
+
+}
